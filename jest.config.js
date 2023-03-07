a9402b3eb8cc55ec488909279454023c3d98c3ce
Ubinquitous <ubinquitous1@gmail.com>
2023-03-07 20:21:21 +0900

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['./src/setupTests.ts'],
+	globals: {
+		'ts-jest': {
+			tsconfig: 'tsconfig.json',
+		},
+	},
+}
