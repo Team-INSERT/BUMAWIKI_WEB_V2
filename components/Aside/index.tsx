c42a73f51a0cbf51db7a74bb2c09ad96e46b324b
Ubinquitous <ubinquitous1@gmail.com>
2023-04-03 23:27:59 +0900

    UPDATE : httpClient 적용, 타입지정 및 변수명 변경

--- components/Aside/index.tsx
+++ components/Aside/index.tsx
@@ -1,20 +1,25 @@
 import * as S from './style'
 import * as FC from '@/utils'
-import * as api from '@/api/getDocs'
 
 import React from 'react'
-import Docs from '@/types/docs.type'
 import PrevLogo from 'assets/prev.svg'
 import NextLogo from 'assets/next.svg'
-import { useQuery } from 'react-query'
+import httpClient from '@/lib/httpClient'
+import Docs from '@/types/docs.type'
 
 const Aside = () => {
 	const [page, setPage] = React.useState(0)
-	const { data, refetch } = useQuery('lastModifiedDocs', () => api.getLastModifiedDocs(page))
+	const [lastModifiedDocs, setLastModifiedDocs] = React.useState<Docs[]>([])
 
 	React.useEffect(() => {
-		refetch()
-		// eslint-disable-next-line
+		;(async () => {
+			try {
+				const res = await httpClient.lastModified.getInQuery('page', page)
+				setLastModifiedDocs(res.data)
+			} catch (err) {
+				console.log(err)
+			}
+		})()
 	}, [page])
 
 	return (
@@ -22,7 +27,7 @@ const Aside = () => {
 			<S.AsideTitleWrap>
 				<S.AsideTitle>최근 수정된 문서</S.AsideTitle>
 			</S.AsideTitleWrap>
-			{data?.map((docs: Docs) => (
+			{lastModifiedDocs.map((docs: Docs) => (
 				<S.AsideDocWrap key={docs.id}>
 					<S.AsideList href={`/docs/${docs.title}`}>{FC.asideFormat(docs.title, docs.docsType)}</S.AsideList>
 					<S.AsideLastModified>&nbsp; {FC.getLastDate(docs.lastModifiedAt)}</S.AsideLastModified>
@@ -33,7 +38,7 @@ const Aside = () => {
 					<S.AsideImage src={PrevLogo} alt="" />
 					<S.AsidePageText>이전</S.AsidePageText>
 				</S.AsidePageButton>
-				<S.AsidePageButton onClick={() => setPage(data.length < 12 ? page : page + 1)}>
+				<S.AsidePageButton onClick={() => setPage(lastModifiedDocs.length < 12 ? page : page + 1)}>
 					<S.AsidePageText>다음</S.AsidePageText>
 					<S.AsideImage src={NextLogo} alt="" />
 				</S.AsidePageButton>
@@ -42,4 +47,4 @@ const Aside = () => {
 	)
 }
 
-export default React.memo(Aside)
+export default Aside
