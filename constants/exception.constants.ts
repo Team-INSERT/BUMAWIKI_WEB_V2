c34869d8093d9cad4e4e5cfe339775be33938b5b
Ubinquitous <ubinquitous1@gmail.com>
2023-04-11 20:01:27 +0900

    ADD : 에러 코드 constants로 정의하여 처리

---
+++ constants/exception.constants.ts
@@ -0,0 +1,15 @@
+const exception = {
+	IMG_400_1: 'IMG-400-1',
+	DOCS_404_1: 'DOCS-404-1',
+	DOCS_404_2: 'DOCS-404-2',
+	DOCS_403_1: 'DOCS-403-1',
+	COMMON_403_1: 'COMMON-403-1',
+	USER_403_1: 'USER-403-1',
+	DOCS_403_2: 'DOCS-403-2',
+	TOKEN_403_1: 'TOKEN-403-1',
+	TOKEN_403_2: 'TOKEN-403-2',
+	TOKEN_403_3: 'TOKEN-403-3',
+	USER_404_1: 'USER-404-1',
+}
+
+export default exception
