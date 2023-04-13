ddf1dd33848fc2ef0fb5acdc7ac20d46e9e37e30
Ubinquitous <ubinquitous1@gmail.com>
2023-04-14 07:52:04 +0900

    ADD : getStaticPaths blocking으로 설정

--- pages/version/[docs]/index.tsx
+++ pages/version/[docs]/index.tsx
@@ -9,9 +9,11 @@ import useConfig from '@/hooks/useConfig'
 interface SingleDocsPropsType {
 	version: VersionDocs[]
 	docsName: string
+	index: number
 }
 
 const Version = (props: SingleDocsPropsType) => {
+	console.log(props)
 	const { seoConfig } = useConfig({
 		title: `부마위키 문서 수정 기록 - ${props.docsName}`,
 		description: `"${props.docsName}" 문서의 수정 기록 페이지입니다.`,
@@ -25,6 +27,13 @@ const Version = (props: SingleDocsPropsType) => {
 	)
 }
 
+export const getStaticPaths = async () => {
+	return {
+		paths: [],
+		fallback: 'blocking',
+	}
+}
+
 export const getStaticProps: GetStaticProps = async (context) => {
 	const { params } = context
 
@@ -34,6 +43,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 		props: {
 			version: res.versionDocsResponseDto,
 			docsName: params?.docs,
+			index: res.versionDocsResponseDto.length,
 		},
 	}
 }
