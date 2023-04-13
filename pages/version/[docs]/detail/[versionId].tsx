43da4db8afe749abda643c67683edd404c552a05
Ubinquitous <ubinquitous1@gmail.com>
2023-04-14 07:52:26 +0900

    FIX : getStaticPaths 설정, API DTO 변경

--- pages/version/[docs]/detail/[versionId].tsx
+++ pages/version/[docs]/detail/[versionId].tsx
@@ -1,44 +1,42 @@
 import React from 'react'
-import { VersionDocsService } from '@/types/version.type'
 import { GetStaticProps } from 'next'
 import { NextSeo } from 'next-seo'
 import httpClient from '@/lib/httpClient'
-import VersionDetailLayout from '@/layout/version/VersionDetailLayout'
 import useConfig from '@/hooks/useConfig'
+import VersionDetailLayout from '@/layout/version/VersionDetailLayout'
+import { VersionDetailPropsType } from '@/types/versionDetail.type'
 
-interface VersionDetailPropsType {
-	title: string
-	docsType: string
-	docs: VersionDocsService
-	versionId: string
-}
-
-const VersionDetail = (props: VersionDetailPropsType) => {
+const VersionDetail = ({ info, versionId, different }: VersionDetailPropsType) => {
 	const { seoConfig } = useConfig({
-		title: `부마위키 문서 기록 - ${props.title}:${props.versionId}`,
-		description: `"${props.title}" 문서의 예전 기록 페이지입니다.`,
+		title: `부마위키 문서 기록 - ${info.title}[${versionId}]`,
+		description: `"${info.title}" 문서의 예전 기록 페이지입니다.`,
 	})
 
 	return (
 		<>
 			<NextSeo {...seoConfig} />
-			<VersionDetailLayout {...props} />
+			<VersionDetailLayout info={info} versionId={versionId} different={different} />
 		</>
 	)
+}
+
+export const getStaticPaths = async () => {
+	return {
+		paths: [],
+		fallback: 'blocking',
+	}
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
 	const { params } = context
 
-	const res = (await httpClient.version.getByTitle((params?.docs as string) || '')).data
-	const versionDocsArray = res.versionDocsResponseDto.reverse()
-
+	const different = (await httpClient.different.getByTitle(`${params?.docs}/different/${params?.versionId}`)).data
+	const info = (await httpClient.version.getByTitle(`${params?.docs}/version`)).data
 	return {
 		props: {
-			title: res.docsResponseDto.title,
-			docsType: res.docsResponseDto.docsType,
-			docs: versionDocsArray[parseInt(params?.versionId as string)],
+			info: info.docsResponseDto,
 			versionId: params?.versionId,
+			different: different.diff,
 		},
 	}
 }
