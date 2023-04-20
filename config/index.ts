f9b2e10e9e2584eef9f4a130a197435583c33338
Ubinquitous <ubinquitous1@gmail.com>
2023-04-20 17:26:00 +0900

    ADD : config

---
+++ config/index.ts
@@ -0,0 +1,8 @@
+const createConfig = () => {
+	return {
+		clientUrl: typeof window !== 'undefined' ? window.location.origin : '',
+		serviceName: 'BUMAWIKI',
+	}
+}
+
+export default createConfig()
