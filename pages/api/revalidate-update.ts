3ca21768341bc29cdfcb1b1a7bc78e30933ce4c9
Ubinquitous <ubinquitous1@gmail.com>
2023-04-20 17:27:49 +0900

    ADD : revalidate api

---
+++ pages/api/revalidate-update.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method !== 'POST') {
+		return res.status(400).json({ error: 'Invalid HTTP method. Only POST method is allowed.' })
+	}
+	try {
+		await res.revalidate(`/update/${req.body.title}`)
+
+		return res.json({ revalidated: true })
+	} catch (err) {
+		return res.status(500).send('Error revalidating')
+	}
+}
