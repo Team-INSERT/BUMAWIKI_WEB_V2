2bdf4f5bccef1acbf43a2050f0162da1cf2575f6
Ubinquitous <ubinquitous1@gmail.com>
2023-04-25 12:33:44 +0900

    SET : 구글 애널리틱스 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import { Aside, Footer, Header, ScrollBtn } from '@/components'
+import { Aside, Footer, Header } from '@/components'
+import React from 'react'
 import { CustomToastContainer } from '@/layout/HomeLayout.style'
 import '@/styles/globals.css'
 import axios from 'axios'
+import * as gtag from '@/utils/etc/gtag'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { RecoilRoot } from 'recoil'
+import Script from 'next/script'
 
 axios.defaults.baseURL = 'https://bumawiki.kro.kr/api'
 
@@ -21,8 +25,38 @@ const queryClient = new QueryClient({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+	const router = useRouter()
+	React.useEffect(() => {
+		const handleRouteChange = (url: any) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		router.events.on('hashChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+			router.events.off('hashChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
 	return (
 		<QueryClientProvider client={queryClient}>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+				}}
+			/>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
 			<RecoilRoot>
 				<CustomToastContainer autoClose={1000} position={toast.POSITION.TOP_RIGHT} />
 				<Header />
