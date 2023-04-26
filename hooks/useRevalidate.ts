11ce941c5893d24f793dfca4e2e11a85248f85f1
Ubinquitous <ubinquitous1@gmail.com>
2023-04-26 14:49:27 +0900

    FIX : reload 개념 수정

--- hooks/useRevalidate.ts
+++ hooks/useRevalidate.ts
@@ -1,12 +1,18 @@
 import config from '@/config'
 import httpClient from '@/lib/httpClient'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
 
 const useRevalidate = (docsName: string) => {
+	const router = useRouter()
+
 	const revalidate = (title = docsName) => {
 		httpClient.revalidateUpdate.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-update` })
 		httpClient.revalidateVersion.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-version` })
 		httpClient.revalidateDocs.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-docs` }).then(() => {
-			window.location.href = `/docs/${title}`
+			router.push(`/docs/${title}`)
+			window.location.reload()
+			toast.success('문서 편집 성공!')
 		})
 	}
 
