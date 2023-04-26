23a8825de3b759f2033ab26bd0c5274363546439
Ubinquitous <ubinquitous1@gmail.com>
2023-04-26 15:19:10 +0900

    HOTFIX : window href

--- hooks/useRevalidate.ts
+++ hooks/useRevalidate.ts
@@ -5,7 +5,9 @@ const useRevalidate = (docsName: string) => {
 	const revalidate = (title = docsName) => {
 		httpClient.revalidateDocs.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-docs` }).then(() => {
 			httpClient.revalidateVersion.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-version` }).then(() => {
-				httpClient.revalidateUpdate.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-update` }).then(() => {})
+				httpClient.revalidateUpdate.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-update` }).then(() => {
+					window.location.href = `/docs/${title}`
+				})
 			})
 		})
 	}
