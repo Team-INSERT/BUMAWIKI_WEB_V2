415829bffba2552d0dab1ac56e15eb2696766fb7
Ubinquitous <ubinquitous1@gmail.com>
2023-04-26 14:45:46 +0900

    FIX : 구조분해할당 실수 수정

--- hooks/useRevalidate.ts
+++ hooks/useRevalidate.ts
@@ -1,12 +1,12 @@
 import config from '@/config'
 import httpClient from '@/lib/httpClient'
 
-const useRevalidate = (title: string) => {
-	const revalidate = (docsName = title) => {
-		httpClient.revalidateUpdate.post({ docsName }, { baseURL: `${config.clientUrl}/api/revalidate-update` })
-		httpClient.revalidateVersion.post({ docsName }, { baseURL: `${config.clientUrl}/api/revalidate-version` })
-		httpClient.revalidateDocs.post({ docsName }, { baseURL: `${config.clientUrl}/api/revalidate-docs` }).then(() => {
-			window.location.href = `/docs/${docsName}`
+const useRevalidate = (docsName: string) => {
+	const revalidate = (title = docsName) => {
+		httpClient.revalidateUpdate.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-update` })
+		httpClient.revalidateVersion.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-version` })
+		httpClient.revalidateDocs.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-docs` }).then(() => {
+			window.location.href = `/docs/${title}`
 		})
 	}
 
