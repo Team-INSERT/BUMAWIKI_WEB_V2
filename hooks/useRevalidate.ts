9d99cfb1d1be80caf8f6b1223e50d4286f04a188
Ubinquitous <ubinquitous1@gmail.com>
2023-04-26 14:51:19 +0900

    FIX : 제발 작동해주세요

--- hooks/useRevalidate.ts
+++ hooks/useRevalidate.ts
@@ -1,18 +1,14 @@
 import config from '@/config'
 import httpClient from '@/lib/httpClient'
-import { useRouter } from 'next/router'
-import { toast } from 'react-toastify'
 
 const useRevalidate = (docsName: string) => {
-	const router = useRouter()
-
 	const revalidate = (title = docsName) => {
-		httpClient.revalidateUpdate.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-update` })
-		httpClient.revalidateVersion.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-version` })
 		httpClient.revalidateDocs.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-docs` }).then(() => {
-			router.push(`/docs/${title}`)
-			window.location.reload()
-			toast.success('문서 편집 성공!')
+			httpClient.revalidateVersion.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-version` }).then(() => {
+				httpClient.revalidateUpdate.post({ title }, { baseURL: `${config.clientUrl}/api/revalidate-update` }).then(() => {
+					window.location.href = `/docs/${title}`
+				})
+			})
 		})
 	}
 
