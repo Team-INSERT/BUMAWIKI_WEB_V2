7cbe148de2b558408d1de7b6940bd3d8d9e03c7c
Ubinquitous <ubinquitous1@gmail.com>
2023-05-05 17:28:02 +0900

    ADD : 마이페이지에서 좋아요한 목록 조회

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -4,19 +4,28 @@ import { NextSeo } from 'next-seo'
 import MyPageLayout from '@/layout/MyPageLayout'
 import { initUserState } from '@/context/userState'
 import useConfig from '@/hooks/useConfig'
-import { GetServerSideProps } from 'next'
 import httpClient from '@/lib/httpClient'
 import MyPageLikeType from '@/types/like.type'
 
-interface MyPagePropsType {
-	likes: MyPageLikeType[]
-}
-
-const MyPage = ({ likes }: MyPagePropsType) => {
+const MyPage = () => {
+	const [likes, setLikes] = React.useState<MyPageLikeType[]>([{ title: '', docsType: '' }])
 	const { user, isLogined, logout } = useUser()
 	const { seoConfig } = useConfig({
 		title: '부마위키 - 마이페이지',
 		description: '부마위키의 마이페이지입니다.',
+	})
+
+	const onEffectGetLikeList = async () => {
+		try {
+			const like = (await httpClient.getLike.get()).data
+			setLikes(like)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	React.useEffect(() => {
+		onEffectGetLikeList()
 	})
 
 	return (
@@ -25,12 +34,6 @@ const MyPage = ({ likes }: MyPagePropsType) => {
 			<MyPageLayout likes={likes} isLogined={isLogined} user={user || initUserState} mutate={logout} />
 		</>
 	)
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-	const likes = (await httpClient.getLike.get()).data
-
-	return { props: { likes } }
 }
 
 export default MyPage
