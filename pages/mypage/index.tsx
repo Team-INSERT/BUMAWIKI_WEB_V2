823d0f2f1711f698142fae985eae73aad6db21bf
Ubinquitous <ubinquitous1@gmail.com>
2023-05-05 17:18:20 +0900

    ADD : like mypage

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,25 +1,36 @@
 import useUser from '@/hooks/useUser'
 import React from 'react'
-import { QueryClient } from 'react-query'
 import { NextSeo } from 'next-seo'
 import MyPageLayout from '@/layout/MyPageLayout'
 import { initUserState } from '@/context/userState'
 import useConfig from '@/hooks/useConfig'
+import { GetServerSideProps } from 'next'
+import httpClient from '@/lib/httpClient'
+import MyPageLikeType from '@/types/like.type'
 
-const MyPage = () => {
+interface MyPagePropsType {
+	likes: MyPageLikeType[]
+}
+
+const MyPage = ({ likes }: MyPagePropsType) => {
 	const { user, isLogined, logout } = useUser()
 	const { seoConfig } = useConfig({
 		title: '부마위키 - 마이페이지',
 		description: '부마위키의 마이페이지입니다.',
 	})
-	const queryClient = new QueryClient()
 
 	return (
 		<>
 			<NextSeo {...seoConfig} />
-			<MyPageLayout isLogined={isLogined} user={user || initUserState} mutate={logout} />
+			<MyPageLayout likes={likes} isLogined={isLogined} user={user || initUserState} mutate={logout} />
 		</>
 	)
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const likes = (await httpClient.getLike.get()).data
+
+	return { props: { likes } }
 }
 
 export default MyPage
