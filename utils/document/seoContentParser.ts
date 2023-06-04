bb44ffcab16da90894243faa70700eaca39cd1d7
우빈 <102154880+Ubinquitous@users.noreply.github.com>
2023-06-04 20:25:39 +0900

    UPDATE : seoContentParser remove tag

--- utils/document/seoContentParser.ts
+++ utils/document/seoContentParser.ts
@@ -1,11 +1,5 @@
 const seoContentParser = (contents: string) => {
-	const seoContents = `${contents
-		.replace(/<br>/gi, ' ')
-		.replace(/<소제목>/gi, '')
-		.replace(/<\/소제목>/gi, ':')
-		.slice(0, 20)}...`
-
-	return seoContents
+  	return contents.replace(/<[^>]*>/g, '');
 }
 
 export default seoContentParser
