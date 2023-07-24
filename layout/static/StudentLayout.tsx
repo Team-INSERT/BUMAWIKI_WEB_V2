35985b2e4f7e99a7c8b2569d5f82f2146abb4f68
Ubinquitous <ubinquitous1@gmail.com>
2023-07-24 21:13:47 +0900

    UPDATE : 학생 문서 UI 최적화

--- layout/static/StudentLayout.tsx
+++ layout/static/StudentLayout.tsx
@@ -9,13 +9,24 @@ import {
 } from "@/components";
 import Docs from "@/types/docs.type";
 import * as S from "./StaticLayout.style";
+import * as util from "@/utils";
+import { decodeContents } from "@/utils/document/requestContents";
 
 interface StudentLayoutPropsType {
   docs: Docs[];
   years: number[];
+  freshman: string;
+  sophomore: string;
+  senior: string;
 }
 
-const StudentLayout = ({ docs, years }: StudentLayoutPropsType) => {
+const StudentLayout = ({
+  docs,
+  years,
+  freshman,
+  sophomore,
+  senior,
+}: StudentLayoutPropsType) => {
   return (
     <S.StaticWrap>
       <Board>
@@ -27,8 +38,29 @@ const StudentLayout = ({ docs, years }: StudentLayoutPropsType) => {
         </S.StaticClassify>
         <S.StaticLine />
         <S.StaticListWrap>
+          <AccodianMenu name={`${new Date().getFullYear()}학년도 1학년`}>
+            <S.StaticGradeBox
+              dangerouslySetInnerHTML={{
+                __html: util.documentation(decodeContents(freshman)),
+              }}
+            ></S.StaticGradeBox>
+          </AccodianMenu>
+          <AccodianMenu name={`${new Date().getFullYear()}학년도 2학년`}>
+            <S.StaticGradeBox
+              dangerouslySetInnerHTML={{
+                __html: util.documentation(decodeContents(sophomore)),
+              }}
+            ></S.StaticGradeBox>
+          </AccodianMenu>
+          <AccodianMenu name={`${new Date().getFullYear()}학년도 3학년`}>
+            <S.StaticGradeBox
+              dangerouslySetInnerHTML={{
+                __html: util.documentation(decodeContents(senior)),
+              }}
+            ></S.StaticGradeBox>
+          </AccodianMenu>
           {years.map((year) => (
-            <AccodianMenu name={`${year}년도 입학생`} key={year} isOpen={true}>
+            <AccodianMenu name={`${year}년도 입학생`} key={year} isOpen={false}>
               {docs.map((student: Docs) => (
                 <S.StaticList key={student.id}>
                   {student.enroll === year && (
